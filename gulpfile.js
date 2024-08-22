35ed175f21020f12aedf5207db5ae09816dd86f3
joserojascascopy <joserojascascopy@hotmail.com>
2024-08-21 20:16:14 -0400

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,16 @@
-export function hola( done ) {
-    console.log("Hola desde gulpfile.js");
-    done();
-};
+import { src, dest, watch} from 'gulp';
+import gulpSass from 'gulp-sass';
+import * as dartSass from 'sass';
+
+const sass = gulpSass(dartSass);
+
+export function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css') )
+    done()
+}
+
+export function dev() {
+    watch('src/scss/app.scss', css)
+}
